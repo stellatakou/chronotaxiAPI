75ed9646874c9647578464d5d17da5d0cef217fd
guyms <guymsfaithful@gmail.com>
2021-02-26 08:25:17 +0100

    fill api

--- routes/api.php
+++ routes/api.php
@@ -22,21 +22,21 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::get('user', 'UsersController@user');
     Route::put("update/{id}", "UsersController@update");
     Route::get("sold", "UsersController@sold");
-
+    
     Route::resource('reviews', 'ReviewController');
     Route::resource('drivers', 'DriverController');
     Route::post('drivers/find', "DriverController@find");
     Route::post("driver/command", "DriverController@command");
-
+    
     Route::get("rides/all", "RidesController@getAllDriverRides");
     Route::get("rides/client/all", "RidesController@getAllClientRides");
-	Route::get("rides/types", "RidesController@getTypes");
-	Route::resource("rides", "RidesController");
+    Route::get("rides/types", "RidesController@getTypes");
+    Route::resource("rides", "RidesController");
     Route::post("rides/bydistance", "RidesController@getRidesByDistance");
     Route::put("rides/accept/{id}", "RidesController@acceptRide");
     Route::get("/rides/{ride_id}/terminate", "RidesController@terminate");
     Route::get("/rides/{ride_id}/notify/client", "RidesController@notifyClient");
-
+    
     Route::post("users/{id}/token/register", "UsersController@registerToken");
     Route::put("users/token/update", "UsersController@updateToken");
 });
